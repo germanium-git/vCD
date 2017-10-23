b6f0b129c14280c404868fef644f08168d4f2cd4
Petr Nemec <petr.nemec@tieto.com>
2017-10-23 15:18:31 +0200

    WIP

---
+++ vcd_getvimServer.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+
+"""
+===================================================================================================
+   Author:         Petr Nemec
+   Description:    Get the vCenter UUID - vimServerReferences
+   Date:           2017-10-23
+===================================================================================================
+"""
+
+from vcd import vCD
+from vcd import credentials
+from vcd import seldc
+import sys
+
+
+# Select the vCD to be modified
+inputs = 'inputs/vcd_' + seldc(sys.argv[1:]) + '.yml'
+
+
+# vCD credentials
+cred = credentials(inputs)
+myvcd = vCD(*cred)
+
+
+# List all external networks
+myvcd.gettoken()
+print(myvcd.getvcenter())
+
+
