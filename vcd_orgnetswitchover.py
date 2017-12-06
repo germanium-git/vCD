e73ad39334f89b2d4a04d566074c1379a220ec36
Petr Nemec <petr.nemec@tieto.com>
2017-12-06 11:31:10 +0100

    nw switchover

--- vcd_orgnetswitchover.py
+++ vcd_orgnetswitchover.py
@@ -110,16 +110,16 @@ while new_vdcnet == 'None' or new_vdcnet not in vdcnets:
 # Check that vApps contains the Old&New Org Networks ------------------------------------------------
 # Identify the current IPs & network cards ----------------------------------------------------------
 
-# Get the list of VMs & IPs -------------------------------------------------------------
+# Get the list of VMs -------------------------------------------------------------------
 
 VMs = {}
 for vapp in vapps:
     VMs.update(myvcd.getvapp_vms(existing_vapps[vapp]))
 
+# Get the list of VMs & IPs -------------------------------------------------------------
 
 VMs_w_ip = {}
 for vm in VMs:
-    pprint(myvcd.getvapp_vm_networkcards(VMs[vm]))
     VMs_w_ip[vm] = VMs[vm]
     VMs_w_ip.update(myvcd.getvapp_vm_networkcards(VMs[vm]))
 
